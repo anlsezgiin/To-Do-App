5090458e8647e069b8001b4781a6f82b8c1f6244
Anıl Sezgin <anlsezgin3@gmail.com>
2023-09-14 20:18:49 +0300

    Basic styles, date function.

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const app = express();
 const port = 3000;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Date section
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const d = new Date();
+let day = d.getDay();
+let date = d.getDate();
+let month = d.getMonth()+1;
+if (month < 10) month = "0" + month;
+let year = d.getFullYear();
+let fullDate= `${weekday[day]},${date}/${month}/${year}`;
+
+
+
+app.get('/', (req, res) =>
+{
+    res.render('index.ejs', 
+    {date: fullDate});
+});
 
 
 app.listen(port, () => 
